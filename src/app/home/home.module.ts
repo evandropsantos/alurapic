50f32252453adb6003fffe78cc05342779176d4a
Evandro Santos <evandro.santos@era-br.com>
2021-08-27 08:31:48 -0300

    chore(login): base validacao

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,15 @@
-import { SignInComponent } from './signin/signin.component'
+import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { ReactiveFormsModule } from '@angular/forms'
 
+import { SignInComponent } from './signin/signin.component'
+import { VMessageModule } from './../shared/components/vmessage/vmessage.module'
 @NgModule({
-  declarations: [ SignInComponent ]
+  declarations: [ SignInComponent ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    VMessageModule
+  ]
 })
 export class HomeModule {}
