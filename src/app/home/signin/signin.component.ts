f3d9ad4ebd4c3d37dc3d6be15f5f84576d287c89
Evandro Santos <evandro.santos@era-br.com>
2021-08-31 09:39:20 -0300

    chore(home): base pagina

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,19 +1,43 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
 
+import { AuthService } from './../../core/auth.service'
 @Component({
   templateUrl: './signin.component.html'
 })
 export class SignInComponent implements OnInit {
 
-  loginForm: FormGroup | any
+  loginForm!: FormGroup | any
+  @ViewChild('userNameInput') userNameInput!: ElementRef<HTMLInputElement>
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required]
     })
+  }
+
+  login() {
+    const userName = this.loginForm.get('userName').value
+    const password = this.loginForm.get('password').value
+
+    this.authService
+      .authenticate(userName, password)
+      .subscribe(
+        () => this.router.navigate(['user', userName]),
+        error => {
+          alert(`Acesso negado -> ${error.message}`)
+
+          this.userNameInput.nativeElement.focus()
+          this.loginForm.reset()
+        }
+      )
   }
 }
